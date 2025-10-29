fc79b92a196313ac38c58f44d03e47439da287d2
tiwilin0323-maker <tiwilin0323@gmail.com>
2025-10-29 13:05:08 +0800

    Add Swagger documentation and test endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SwaggerController;
+use App\Http\Controllers\TestController;
 
 $app->router()->get('/', HomeController::class);
 $app->router()->get('/api/ping', [HomeController::class, 'ping']);
+$app->router()->get('/api/test', [TestController::class, 'index']);
+$app->router()->get('/swagger.json', [SwaggerController::class, 'json']);
+$app->router()->get('/docs', [SwaggerController::class, 'ui']);
